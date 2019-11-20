4085bd64da9aef76b9dc3caf0c318c85ca2a0da3
ChrisConcho <concho@usc.edu>
2019-11-20 15:01:34 -0800

    Testing

---
+++ lab12/bst.cpp
@@ -0,0 +1,74 @@
+#include "bst.h"
+
+#include <stack>
+#include <queue>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+//Helper functions?
+bool isBST(Node *root) {
+    int flag = 0;
+    if ( root == nullptr){
+        return true;
+    }
+    else if (isBST(root->left)){
+        
+        if (root->left->key < root->key){
+            flag += 1;
+        }
+        else{
+            return false;
+            }
+    }
+    else if (isBST(root->right)){
+        if(root->right->key > root->key){
+            flag += 1;
+        }
+        else{
+            return false;
+        }
+    }
+    if ( flag == 2){
+        return true;
+    }
+    return false;
+}
+    
+//Helper functions?
+bool isBalanced(Node *root) {
+    if ( helper(root,1) == -1){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+int helper(Node* check, int x){
+    if ( check == nullptr){
+        return x;
+    }
+    else{
+        int left = helper(check->left, x+1);
+        int right = helper(check->right, x+1);
+        if ( left == -1 || right == -1){
+            return -1;
+        }
+        else if ( left - right >= 1 || left-right< -1){
+            return -1;
+        }
+        else{
+            if ( left>right){
+                return left;
+            }
+            else{
+                return right;
+            }
+        }
+}
